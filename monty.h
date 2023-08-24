cccf5a88c2b5017f80b1ca86b9111cb23571e550
LAKRIM Ossama <lakrimoussama@gmail.com>
2023-08-23 18:43:34 -0700

    This is our header file for this project

---
+++ monty.h
@@ -0,0 +1,73 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct data_s - stocks data program, variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct data_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  data_t;
+extern data_t data;
+
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
+void free_stack(stack_t *top);
+void queue(stack_t **top, unsigned int counter);
+void addqueue(stack_t **top, int n);
+void addnode(stack_t **top, int n);
+
+
+void _0_push(stack_t **top, unsigned int number);
+void _0_pall(stack_t **top, unsigned int number);
+void _1_pint(stack_t **top, unsigned int number);
+
+
+#endif
+
